aed4262496030569ebbc46dba9e5eda4575ed221
DoomPI <rslomtev@edu.hse.ru>
2023-03-19 21:36:05 +0300

    Initial Commit

---
+++ GetSms/GetSmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetSmsApp.swift
+//  GetSms
+//
+//  Created by Роман Ломтев on 19.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct GetSmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
