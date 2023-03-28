243dc5dfd3671aff7709200c57cffd936525da2a
DoomPI <rslomtev@edu.hse.ru>
2023-03-28 19:32:21 +0300

    Теперь на старте подгружаются только российские сервисы

--- GetSms/Features/ServiceList/Data/Workers/Network/ServiceListNetworkWorker.swift
+++ GetSms/Features/ServiceList/Data/Workers/Network/ServiceListNetworkWorker.swift
@@ -18,17 +18,30 @@ class ServiceListNetworkWorker {
     // MARK: - Internal vars
     private let worker = NetworkWorker.shared
     private let decoder = JSONDecoder()
-    private let serviceListUrl = URL(string: "https://vak-sms.com/api/getCountNumbersList/")!
+    private let serviceListUrl = "https://vak-sms.com/api/getCountNumbersList/"
 }
 
 extension ServiceListNetworkWorker: ServiceListNetworkWorkingLogic {
     
-    func getServiceList() -> Single<ServiceListNetworkDTO> {
-        return worker.sendRequest(url: serviceListUrl).map { data in
-            return try self.decoder.decode(
-                ServiceListNetworkDTO.self,
-                from: data
-            )
+    func getServiceList() -> Single<ServiceListNetworkDTO>{
+        Single.deferred { [weak self] in
+            guard let self else { throw NetworkError.IrrelevantRequest }
+            let queryItems = [URLQueryItem(name: "country", value: "ru")]
+            var urlComps = URLComponents(string: self.serviceListUrl)!
+            urlComps.queryItems = queryItems
+            
+            return self.worker.sendRequest(
+                url: urlComps.url!
+            ).map { data in
+                try self.decoder.decode(
+                    ServiceListNetworkDTO.self,
+                    from: data
+                )
+            }
         }
     }
+}
+
+enum NetworkError: Error {
+case IrrelevantRequest
 }
