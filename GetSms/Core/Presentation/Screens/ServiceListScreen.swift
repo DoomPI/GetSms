27bd2e6b0cf3ea5580b3b63f8bbb8f74dfb34429
Rammdakk <Rammdakk@gmail.com>
2023-04-02 17:47:27 +0300

    ServiceListScreen added

---
+++ GetSms/Core/Presentation/Screens/ServiceListScreen.swift
@@ -0,0 +1,32 @@
+//
+//  ServiceListScreen.swift
+//  GetSms
+//
+//  Created by Роман Ломтев on 01.04.2023.
+//
+ 
+import SwiftUI
+ 
+struct ServiceListScreen: View {
+ 
+    @ObservedObject var countryListViewModel: CountryListViewModel
+    @ObservedObject var serviceListViewModel: ServiceListViewModel
+ 
+    var body: some View {
+        VStack {
+ 
+            CountryListView()
+                .environmentObject(countryListViewModel)
+ 
+            ServiceListView()
+                .environmentObject(serviceListViewModel)
+        }
+        .padding(8)
+        .background(Color("DarkBlueColor"))
+        .onReceive(countryListViewModel.$state) { newState in
+            if case .Loaded(let vo) = newState{
+                serviceListViewModel.loadServiceList(countryCode: vo.countries[vo.selectedCountryIndex].code)
+            }
+        }
+    }
+}
