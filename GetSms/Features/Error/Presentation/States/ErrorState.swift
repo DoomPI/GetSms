2902787a2d3f8de0ea1e7716e634313700cf75e3
Rammdakk <rammdakk@gmail.com>
2023-05-09 11:41:11 +0300

    Добавлено окно показа ошибки

---
+++ GetSms/Features/Error/Presentation/States/ErrorState.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorState.swift
+//  GetSms
+//
+//  Created by Рамиль Зиганшин on 27.04.2023.
+//
+
+enum ErrorState {
+    
+    case Error(message: String)
+    
+    case None
+}
---
+++ GetSms/Features/Error/Presentation/Views/ErrorView.swift
@@ -0,0 +1,34 @@
+//
+//  ErrorView.swift
+//  GetSms
+//
+//  Created by Рамиль Зиганшин on 27.04.2023.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    
+    @Binding var errorState: ErrorState
+    
+    var body: some View {
+        VStack {
+            switch errorState {
+            case .Error(let message):
+                Text(message)
+                    .frame(maxWidth: .infinity)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 15)
+                    .background(.red)
+                    .cornerRadius(15)
+                    .padding(.horizontal, 10)
+            case .None:
+                EmptyView()
+            }
+            Spacer()
+        } .padding(.vertical, 15)
+    }
+}
+
+
